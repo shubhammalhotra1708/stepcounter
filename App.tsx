8cad7e70feafeffbe621b75b3849e7dc542d5c1c
Shubham Malhotra <shubham@Jitvans-MacBook-Pro.local>
2023-06-25 18:13:17 +0530

    android dev client integrated

--- App.tsx
+++ App.tsx
@@ -2,29 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Value from './src/components/Value';
 import RingProgress from './src/components/RingProgress';
-//import useHealthData from './src/hooks/useHealthData';
-import {useState,useEffect} from 'react';
+import {useState} from 'react';
 import { AntDesign } from '@expo/vector-icons';
 import useHealthData from './src/hooks/useHealthData';
 
 const STEPS_GOAL = 10_000;
 
 export default function App() {
-  // const [date, setDate] = useState(new Date());
-  const { steps, flights, distance } = useHealthData(new Date(2023,5,23));
-  // const changeDate = (numDays : number) => {
-  //   const currentDate = new Date(date); // Create a copy of the current date
-  //   // Update the date by adding/subtracting the number of days
-  //   currentDate.setDate(currentDate.getDate() + numDays);
+   const [date, setDate] = useState(new Date());
+  const { steps, flights, distance } = useHealthData(date);
+  const changeDate = (numDays : number) => {
+    const currentDate = new Date(date);
+    currentDate.setDate(currentDate.getDate() + numDays);
 
-  //   setDate(currentDate); // Update the state variable
-  // };
+    setDate(currentDate); // Update the state variable
+  };
 
   
 
   return (
     <View style={styles.container}>
-      {/* <View style={styles.datePicker}>
+      <View style={styles.datePicker}>
         <AntDesign
           onPress={() => changeDate(-1)}
           name="left"
@@ -39,7 +37,7 @@ export default function App() {
           size={20}
           color="#C3FF53"
         />
-      </View> */}
+      </View>
 
       <RingProgress progress={steps/STEPS_GOAL} radius={150} strokeWidth={50} />
       <View style={styles.values}>
--- src/hooks/useHealthData.tsx
+++ src/hooks/useHealthData.tsx
@@ -1,84 +1,149 @@
-
-import {useState,useEffect} from 'react';
 import AppleHealthKit, {
-    HealthValue,
-    HealthKitPermissions,
-    HealthInputOptions,
-    HealthUnit,
-  } from 'react-native-health'
+  HealthInputOptions,
+  HealthKitPermissions,
+} from 'react-native-health';
+import { useEffect, useState } from 'react';
+import { Platform } from 'react-native';
 
-const useHealthData = (date : Date) => {
-  const [permi,setPermi] = useState(false);
-  const [steps,setSteps] = useState(0);
-  const [flights,setFlights] = useState(0);
-  const [distance,setDistance] = useState(0);
-  const permissions : HealthKitPermissions = {
-    permissions: {
-      read:[AppleHealthKit.Constants.Permissions.Steps ,AppleHealthKit.Constants.Permissions.FlightsClimbed, AppleHealthKit.Constants.Permissions.DistanceWalkingRunning ],
-      write:[],
-    },
-  }
+import {
+  initialize,
+  requestPermission,
+  readRecords,
+} from 'react-native-health-connect';
+import { TimeRangeFilter } from 'react-native-health-connect/lib/typescript/types/base.types';
 
-  AppleHealthKit.isAvailable(()=>{});
+const permissions: HealthKitPermissions = {
+  permissions: {
+    read: [
+      AppleHealthKit.Constants.Permissions.Steps,
+      AppleHealthKit.Constants.Permissions.FlightsClimbed,
+      AppleHealthKit.Constants.Permissions.DistanceWalkingRunning,
+    ],
+    write: [],
+  },
+};
 
-  useEffect(()=>{
-    AppleHealthKit.initHealthKit(permissions, (err)=>{
-      if(err){
-        console.log("error getting permi");
-        return;
-      }
-      setPermi(true);
-    });
-  },[]);
+const useHealthData = (date: Date) => {
+  const [hasPermissions, setHasPermission] = useState(false);
+  const [steps, setSteps] = useState(0);
+  const [flights, setFlights] = useState(0);
+  const [distance, setDistance] = useState(0);
 
-  
-  
-  useEffect(()=>{
-    if(!permi){
+  // iOS - HealthKit
+  useEffect(() => {
+    if (Platform.OS !== 'ios') {
       return;
     }
-    //date to fetch data
-    const options : HealthInputOptions= {
-      date:date.toISOString(),
-      includeManuallyAdded : false,
-      //unit:HealthUnit.meter,
-    }
-    console.log(new Date())
 
-    //fetch STEPS
-    AppleHealthKit.getStepCount(options,(err,result) =>{
-      if(err) {
-        console.log("error getting response");
+    AppleHealthKit.isAvailable((err, isAvailable) => {
+      if (err) {
+        console.log('Error checking availability');
         return;
       }
-      console.log(result.value);
-      setSteps(result.value);
+      if (!isAvailable) {
+        console.log('Apple Health not available');
+        return;
+      }
+      AppleHealthKit.initHealthKit(permissions, (err) => {
+        if (err) {
+          console.log('Error getting permissions');
+          return;
+        }
+        setHasPermission(true);
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!hasPermissions) {
+      return;
+    }
+
+    const options: HealthInputOptions = {
+      date: date.toISOString(),
+      includeManuallyAdded: false,
+    };
+
+    AppleHealthKit.getStepCount(options, (err, results) => {
+      if (err) {
+        console.log('Error getting the steps');
+        return;
+      }
+      setSteps(results.value);
     });
 
-    //fetch FLIGHTS CLIMBED
-    AppleHealthKit.getFlightsClimbed(options,(err,result) =>{
-      if(err) console.log("error getting response");
-
-      console.log(result.value);
-      setFlights(result.value);
+    AppleHealthKit.getFlightsClimbed(options, (err, results) => {
+      if (err) {
+        console.log('Error getting the steps:', err);
+        return;
+      }
+      setFlights(results.value);
     });
 
-    //fetch DISTANCE
-    AppleHealthKit.getDistanceWalkingRunning(options,(err,result) =>{
-      if(err) console.log("error getting response");
-
-      console.log(result.value);
-      setDistance(result.value);
+    AppleHealthKit.getDistanceWalkingRunning(options, (err, results) => {
+      if (err) {
+        console.log('Error getting the steps:', err);
+        return;
+      }
+      setDistance(results.value);
     });
+  }, [hasPermissions, date]);
 
-  },[permi]);
+  // Android - Health Connect
+  const readSampleData = async () => {
+    // initialize the client
+    const isInitialized = await initialize();
+    if (!isInitialized) {
+      return;
+    }
 
-return {
+    // request permissions
+    await requestPermission([
+      { accessType: 'read', recordType: 'Steps' },
+      { accessType: 'read', recordType: 'Distance' },
+      { accessType: 'read', recordType: 'FloorsClimbed' },
+    ]);
+
+    const timeRangeFilter: TimeRangeFilter = {
+      operator: 'between',
+      startTime: new Date(date.setHours(0, 0, 0, 0)).toISOString(),
+      endTime: new Date(date.setHours(23, 59, 59, 999)).toISOString(),
+    };
+
+    // Steps
+    const steps = await readRecords('Steps', { timeRangeFilter });
+    const totalSteps = steps.reduce((sum, cur) => sum + cur.count, 0);
+    setSteps(totalSteps);
+
+    // Distance
+    const distance = await readRecords('Distance', { timeRangeFilter });
+    const totalDistance = distance.reduce(
+      (sum, cur) => sum + cur.distance.inMeters,
+      0
+    );
+    setDistance(totalDistance);
+
+    // Floors climbed
+    const floorsClimbed = await readRecords('FloorsClimbed', {
+      timeRangeFilter,
+    });
+    const totalFloors = floorsClimbed.reduce((sum, cur) => sum + cur.floors, 0);
+    setFlights(totalFloors);
+    // console.log(floorsClimbed);
+  };
+
+  useEffect(() => {
+    if (Platform.OS !== 'android') {
+      return;
+    }
+    readSampleData();
+  }, [date]);
+
+  return {
     steps,
     flights,
     distance,
-}
+  };
 };
 
 export default useHealthData;
-  
